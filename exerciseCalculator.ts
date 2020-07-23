400975cc55694af274f2f35de38c80c551be934c
Eljoey <eljoeyy@gmail.com>
2020-07-23 18:13:18 -0500

    9.7

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,78 +1,78 @@
 interface ExerciseValues {
-  exerciseHourValues: Array<number>
-  targetValue: number
+  exerciseHourValues: Array<number>;
+  targetValue: number;
 }
 
 interface Result {
-  periodLength: number
-  trainingDays: number
-  success: boolean
-  rating: number
-  ratingDescription: string
-  target: number
-  average: number
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
 }
 
 const processArgs = (args: Array<string>): ExerciseValues => {
-  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length < 4) throw new Error('Not enough arguments');
 
   for (let i = 2; i < args.length; i++) {
     if (isNaN(Number(args[i]))) {
-      throw new Error('Provided values were not numbers!')
+      throw new Error('Provided values were not numbers!');
     }
   }
 
-  const target = Number(args[2])
-  const hours = args.slice(3).map((num) => Number(num))
+  const target = Number(args[2]);
+  const hours = args.slice(3).map((num) => Number(num));
 
   return {
     targetValue: target,
     exerciseHourValues: hours,
-  }
-}
+  };
+};
 
 const calculateExercises = (
   exerciseHours: Array<number>,
   target: number
 ): Result => {
-  const days = exerciseHours.length
-  const hoursTrained = exerciseHours.reduce((acc, cur) => acc + cur)
-  const hoursPerDay = hoursTrained / days
-  const trainingSuccess = hoursPerDay >= target ? true : false
+  const days = exerciseHours.length;
+  const hoursTrained = exerciseHours.reduce((acc, cur) => acc + cur);
+  const hoursPerDay = hoursTrained / days;
+  const trainingSuccess = hoursPerDay >= target ? true : false;
 
   const daysTrained = (hours: Array<number>): number => {
-    let countedDays = 0
+    let countedDays = 0;
     for (let i = 0; i < hours.length; i++) {
       if (hours[i] !== 0) {
-        countedDays++
+        countedDays++;
       }
     }
-    return countedDays
-  }
+    return countedDays;
+  };
 
   const calcRating = (): number => {
-    const percentageOfTarget = (hoursPerDay / target) * 100
+    const percentageOfTarget = (hoursPerDay / target) * 100;
 
     if (percentageOfTarget < 80) {
-      return 1
+      return 1;
     } else if (percentageOfTarget >= 80 && percentageOfTarget < 100) {
-      return 2
+      return 2;
     } else {
-      return 3
+      return 3;
     }
-  }
+  };
 
   const getRatingDescription = (): string => {
-    let rating = calcRating()
+    const rating = calcRating();
 
     if (rating === 1) {
-      return 'You missed the target badly, stay motivated!'
+      return 'You missed the target badly, stay motivated!';
     } else if (rating === 2) {
-      return 'Not bad, but could be better'
+      return 'Not bad, but could be better';
     } else {
-      return 'Great work! You passed your target!'
+      return 'Great work! You passed your target!';
     }
-  }
+  };
 
   return {
     periodLength: days,
@@ -82,14 +82,14 @@ const calculateExercises = (
     ratingDescription: getRatingDescription(),
     target: target,
     average: hoursPerDay,
-  }
-}
+  };
+};
 
 try {
-  const { exerciseHourValues, targetValue } = processArgs(process.argv)
-  console.log(calculateExercises(exerciseHourValues, targetValue))
+  const { exerciseHourValues, targetValue } = processArgs(process.argv);
+  console.log(calculateExercises(exerciseHourValues, targetValue));
 } catch (e) {
-  console.log('Oh my there was an error: ', e.message)
+  console.log('Oh my there was an error: ', e.message);
 }
 
-export { calculateExercises }
+export { calculateExercises };
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { caclulateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.send('pong');
@@ -27,6 +30,39 @@ app.get('/bmi', (req, res) => {
     height,
     bmi,
   });
+});
+
+app.get('/exercises', (req, res) => {
+  const daily_exercises = req.body.daily_exercises;
+  const target = req.body.target;
+
+  // ensure all daily_exercise is a number
+  const daily_exercises_formatted = [];
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  for (let i = 0; i < daily_exercises.length; i++) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    daily_exercises_formatted.push(Number(daily_exercises[i]));
+  }
+
+  const checkDailyExercises = (array: Array<number | string>): boolean => {
+    for (let i = 0; i < array.length; i++) {
+      console.log(array[i]);
+      if (!array[i] && array[i] !== 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+  if (!daily_exercises || !target) {
+    return res.json({ error: 'parameters missing' });
+  }
+  if (!Number(target) || !checkDailyExercises(daily_exercises_formatted)) {
+    return res.json({ error: 'malformatted parameters' });
+  }
+
+  const exerciseResults = calculateExercises(daily_exercises_formatted, target);
+
+  return res.json(exerciseResults);
 });
 
 const PORT = 3003;
